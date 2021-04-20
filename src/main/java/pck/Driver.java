2bcc1cf185743605f798af978cfb7a9d430b0f4f
Andrea <andreasin88@gmail.com>
2021-04-20 19:52:44 +0200

    update

--- src/main/java/pck/Driver.java
+++ src/main/java/pck/Driver.java
@@ -1,16 +1,21 @@
 package pck;
 
-public class Driver {
+import java.util.Optional;
 
-	public static void main(String[] args) {
+public class Driver {
+	public static void main(String[] args) throws Throwable {
 	
 		BookDB bookDB = new BookDB();
-		bookDB.add(new Book(2,"titolo2"));
-		
-		if(bookDB.getBookById(3).isEmpty()) {
+		bookDB.add(new Book(3,"titolo2"));
+		Optional<?> opt = bookDB.getBookById(2);
+		if(opt.isEmpty()) {		
+			// orElse permettere di usare un altro valore in caso opt fosse vuoto (in questo caso magari un altro libro)
+			// opt.orElse(opt);
 			System.out.println("no book found");
-		} else {
+			System.out.println(opt.orElseThrow(ArithmeticException::new));
+		} else if (opt.isPresent()) {
 			System.out.println("book found!");
+			System.out.println(((Book) opt.get()).getTitle());
 		}
 	}
 
